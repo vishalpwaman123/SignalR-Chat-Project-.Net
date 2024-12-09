05a6aeacf39e4b731b3f6eb74ffc6b9f41ca9fa9
vishalpwaman123 <vishalpwaman123@gmail.com>
2024-12-09 16:41:13 +0530

    Added : Group chat code

--- SignalR.Chat.Application/Hubs/ChatHub.cs
+++ SignalR.Chat.Application/Hubs/ChatHub.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
+using SignalR.Chat.Application.Model;
 using SignalR.Chat.Application.Services;
 
 namespace SignalR.Chat.Application.Hubs
@@ -23,12 +24,28 @@ namespace SignalR.Chat.Application.Hubs
         public override async Task OnDisconnectedAsync(Exception ex)
         {
             await Groups.RemoveFromGroupAsync(Context.ConnectionId, "Come2Chat");
+            var user = _chatService.GetUserByConnectionId(Context.ConnectionId);
+            _chatService.RemoveUserFromList(user);
+            await DisplayOnlineUsers();
             await base.OnDisconnectedAsync(ex);
         }
 
-        /*public async Task AddUserConnectionId(string userName)
+        public async Task AddUserConnectionId(string userName)
         {
             _chatService.AddUserConnectionId(userName, Context.ConnectionId);
-        }*/
+            var onlineUsers = _chatService.GetOnlineUsers();
+            await Clients.Groups("Come2Chat").SendAsync("OnlineUsers", onlineUsers);
+        }
+
+        private async Task DisplayOnlineUsers()
+        {
+            var onlineUsers = _chatService.GetOnlineUsers();
+            await Clients.Groups("Come2Chat").SendAsync("OnlineUsers", onlineUsers);
+        }
+
+        public async Task ReceiveMessage(MessageDto message)
+        {
+            await Clients.Group("Come2Chat").SendAsync("NewMessage", message);
+        }
     }
 }
---
+++ SignalR.Chat.Application/Model/MessageDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SignalR.Chat.Application.Model
+{
+    public class MessageDto
+    {
+        [Required]
+        public string From { get; set; }
+        public string To { get; set; }
+        [Required]
+        public string Content { get; set; }
+    }
+}
